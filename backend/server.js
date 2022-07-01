594f69376dd96c95931f9d3bcee160e95143e8ff
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2022-07-01 09:38:11 +0500

    Initialized Backend and Frontend
    Initialized Backend and Frontend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Init Middleware
+app.use(express.json({ extended: false}));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+// Define Routes here
+app.use('/cars', require('./routes/cars'));
+
+app.listen(PORT, () => console.log(`The Server started on port ${PORT}`));
